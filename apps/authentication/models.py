874d38407a3fdee925cef48cc8349dbf0c1de0e2
Ahmad Mulfasa <dairobi456.jkt@gmail.com>
2024-10-02 17:47:39 +0700

    Set up Database and fix login

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -11,13 +11,15 @@ from apps.authentication.util import hash_pass
 
 class Users(db.Model, UserMixin):
 
-    __tablename__ = 'Users'
+    __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True)
+    # id = db.Column(db.Integer, primary_key=True)
+    userID = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True)
     email = db.Column(db.String(64), unique=True)
     password = db.Column(db.LargeBinary)
-
+    id = userID
+    
     def __init__(self, **kwargs):
         for property, value in kwargs.items():
             # depending on whether value is an iterable or not, we must
